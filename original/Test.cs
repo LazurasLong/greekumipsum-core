452d5465856d8d5bb3aca3d46cf23e966abcd6d3
Palaiologos <marekszuwarek1958@gmail.com>
2018-08-24 15:20:21 +0200

    Create Test.cs

---
+++ original/Test.cs
@@ -0,0 +1,10 @@
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Test {
+    public static void Main(string[] args) {
+        Console.Write(new GreekumIpsum().NextParagraph());
+    }
+}
