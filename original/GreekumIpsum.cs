41cb6d7cf883c8c1cd53a022fd66df2be7dafe91
Palaiologos <marekszuwarek1958@gmail.com>
2018-08-25 16:16:09 +0200

    Update original

--- original/GreekumIpsum.cs
+++ original/GreekumIpsum.cs
@@ -9,38 +9,53 @@ class GreekumIpsum {
     
     private char lastAlphabetLetter;
     private Random randomGenerator = new Random();
+    
+    private bool startingZero = false;
+    private bool errored = false;
 
     public char NextCharacter() {
-        double r = randomGenerator.NextDouble(), sum = 0;
-        int idx = 0;
-        foreach (char n in alphabet.GetAlphabet()) {
-            if (r <= (sum = sum + probability.GetLetterProbability(alphabet)[idx++])) {
-                if (lastAlphabetLetter == n) return NextCharacter();
-                lastAlphabetLetter = n;
-                return n;
+        try {
+            double r = randomGenerator.NextDouble(), sum = 0;
+            int idx = 0;
+            foreach (char n in alphabet.GetAlphabet()) {
+                if (r <= (sum = sum + probability.GetLetterProbability(alphabet)[idx++])) {
+                    if (lastAlphabetLetter == n) return NextCharacter();
+                    lastAlphabetLetter = n;
+                    return n;
+                }
             }
+            errored = true;
+        } catch(Exception e) {
+            errored = true;
         }
+        
         return '?';
     }
 
     public string NextWord() {
-        double runningtotal = 0.0, randValue = randomGenerator.NextDouble();
-        int length = 5, idx = 0;
-        string word = "";
+        try {
+            double runningtotal = 0.0, randValue = randomGenerator.NextDouble();
+            int length = 5, idx = 0;
+            string word = "";
 
-        foreach (int obj in wordPropertyProvider.GetWordLengths()) {
-            runningtotal += probability.GetWordProbability(wordPropertyProvider)[idx];
-            if (runningtotal > randValue) {
-                length = obj;
-                break;
+            foreach (int obj in wordPropertyProvider.GetWordLengths()) {
+                runningtotal += probability.GetWordProbability(wordPropertyProvider)[idx];
+                if (runningtotal > randValue) {
+                    length = obj;
+                    break;
+                }
+                idx++;
             }
-            idx++;
+            
+            for (int i = 0; i < length; i++) {
+                word += Char.ToLower(NextCharacter());
+            }
+            return word;
+        } catch(Exception e) {
+            errored = true;
         }
         
-        for (int i = 0; i < length; i++) {
-            word += Char.ToLower(NextCharacter());
-        }
-        return word;
+        return "???";
     }
     
     public string NextWord(int amount) {
@@ -53,30 +68,36 @@ class GreekumIpsum {
     }
 
     public string NextSentence(bool isStartOfWord) {
-        string sentence = "";
-        int length = randomGenerator.Next(2,6);
-        if(isStartOfWord) {
-            length--;
-            char[] wrd = NextWord().ToCharArray();
-            wrd[0] = Char.ToUpper(wrd[0]);
-            sentence += new string(wrd);
-            sentence += " ";
-        }
-        for(int idx = 0; idx < length; idx++) {
-            sentence += NextWord();
-            sentence += " ";
-        }
+        try {
+            string sentence = "";
+            int length = randomGenerator.Next(2,6);
+            if(isStartOfWord) {
+                length--;
+                char[] wrd = NextWord().ToCharArray();
+                wrd[0] = Char.ToUpper(wrd[0]);
+                sentence += new string(wrd);
+                sentence += " ";
+            }
+            for(int idx = 0; idx < length; idx++) {
+                sentence += NextWord();
+                sentence += " ";
+            }
 
-        sentence = sentence.Remove(sentence.Length - 1);
+            sentence = sentence.Remove(sentence.Length - 1);
+            
+            if(randomGenerator.Next() % 2 == 0) {
+                sentence += ". ";
+            } else {
+                sentence += ", ";
+                sentence += NextSentence(false);
+            }
+
+            return sentence;
+        } catch(Exception e) {
+            errored = true;
+        }
         
-        if(randomGenerator.Next() % 2 == 0) {
-            sentence += ". ";
-        } else {
-            sentence += ", ";
-            sentence += NextSentence(false);
-        }
-
-        return sentence;
+        return "???";
     }
     
     public string NextSentence(bool isStartOfWord, int amount) {
@@ -87,12 +108,33 @@ class GreekumIpsum {
     }
     
     public string NextParagraph() {
-        return "\t" + NextSentence(true, randomGenerator.Next() % 15 + 12);
+        try {
+            return "\t" + NextSentence(true, randomGenerator.Next() % 15 + 12);
+        } catch(Exception e) {
+            errored = true;
+        }
+        
+        return "???";
+    }
+    
+    public bool HasErrored() {
+        return errored;
+    }
+    
+    public void ClearError() {
+        errored = false;
     }
 
-    public GreekumIpsum () {
-        alphabet = new GreekAlphabet();
-        wordPropertyProvider = new GreekWordPropertyProvider();
-        probability = new GreekProbability();
+    public GreekumIpsum (bool startClean) {
+        if(!startClean) {
+            alphabet = new GreekAlphabet();
+            wordPropertyProvider = new GreekWordPropertyProvider();
+            probability = new GreekProbability();
+        } else {
+            alphabet = new CleanGreekAlphabet();
+            wordPropertyProvider = new CleanGreekWordPropertyProvider();
+            probability = new GreekProbability();
+            startingZero = true;
+        }
     }
 }
