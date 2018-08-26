897e7aea1e14a7d6d4775f437a6400bc11f54630
Palaiologos <marekszuwarek1958@gmail.com>
2018-08-26 15:57:52 +0200

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GreekumIpsum core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GreekumIpsum")]
+[assembly: AssemblyCopyright("Copyright © 2018 KrzysztofSzewczyk")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a9cd761-692f-40db-8566-c0217e5d3e0a")]
+
+[assembly: AssemblyVersion("2018.8.*")]
+[assembly: AssemblyFileVersion("2018.8.26.12")]
